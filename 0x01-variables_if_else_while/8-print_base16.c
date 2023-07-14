ba0ffe9e3c473b9f5334cd8afc6e0bb3117b695c
Mohamed <mdahni06@gmail.com>
2023-07-15 00:54:43 +0100

    added 8-print_base16.c program

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,20 +7,20 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 16; i++)
-    {
-        if (i < 10)
-        {
-            putchar('0' + i);
-        }
-        else
-        {
-            putchar('a' + (i - 10));
-        }
-    }
-    putchar('\n');
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+		{
+			putchar('0' + i);
+		}
+		else
+		{
+			putchar('a' + (i - 10));
+		}
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
