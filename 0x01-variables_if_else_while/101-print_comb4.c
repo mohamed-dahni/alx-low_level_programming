65564e5aa700077819b664a595bf478a1f4ae604
Mohamed <mdahni06@gmail.com>
2023-07-15 01:31:54 +0100

    added 101-print_comb4.c program

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print all possible combinations of three digits
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+
+				if (i < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
