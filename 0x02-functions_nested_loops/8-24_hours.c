31286ec0a776ad926b74ea8c4f538d9810930cb6
Mohamed <mdahni06@gmail.com>
2023-07-18 23:35:11 +0100

    added 7-print_last_digit.c program

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+/**
+ * print_num - prints a number
+ * @n: number to print
+ */
+void print_num(int n)
+{
+	int last_digit = 0;
+	int first_digit = 0;
+
+	if (n > 10)
+	{
+		last_digit = n % 10;
+		first_digit = (n - last_digit) / 10;
+	} else
+	{
+		last_digit = n;
+	}
+
+	_putchar(first_digit + '0');
+	_putchar(last_digit + '0');
+}
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ */
+void jack_bauer(void)
+{
+	int h, m, first_num_h, second_num_h, first_num_m, second_num_m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			// hours
+			print_num(h);
+			
+			// colon
+			_putchar(':');
+
+			// minuts
+			print_num(m);	
+			
+			// newline
+			_putchar('\n');
+		}
+	}
+}
+
+int main() {
+	jack_bauer();
+	return 0;
+}
