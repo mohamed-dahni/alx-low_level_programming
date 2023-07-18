b0db509276f1cb9d18c6f3d1bc7ffb66cc150cf3
Mohamed <mdahni06@gmail.com>
2023-07-18 22:23:06 +0100

    added 7-print_last_digit.c program

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,9 +5,10 @@
  * @n: number to be checked
  * Return: last digit of n
  */
-int print_last_digit(int n)
+void print_last_digit(int n)
 {
 	int last_digit = n % 10;
 
-	return ((last_digit) < 0 ? -last_digit : last_digit);
+	_putchar(last_digit + '0');
 }
+
