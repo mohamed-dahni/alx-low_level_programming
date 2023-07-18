25a0aa278962b932a9b6f0c6e0be7ff65814795c
Mohamed <mdahni06@gmail.com>
2023-07-18 21:03:53 +0100

    added 0x02-functions_nested_loops program

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c[] = "_putchar\n";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	return (0);
+}
