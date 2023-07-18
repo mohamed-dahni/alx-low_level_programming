2a6d9fc1c8953299aad8b8e2a6aca0c9f8f428a0
Mohamed <mdahni06@gmail.com>
2023-07-18 21:39:15 +0100

    added 2-print_alphabet_x10.c program

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,6 @@
 
 /**
  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
- * Description: Prints the alphabet in lowercase
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
