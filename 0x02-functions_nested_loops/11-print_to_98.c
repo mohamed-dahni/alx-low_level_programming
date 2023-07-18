5fd70a9b7ad51ed9961e8efdca8837cbe0837618
Mohamed <mdahni06@gmail.com>
2023-07-19 00:06:37 +0100

    added 11-print_to_98.c program

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,8 +7,8 @@
 void print_to_98(int n)
 {
 	int i;
-	
-	if(n > 98)
+
+	if (n > 98)
 	{
 		for (i = n; i > 98; i--)
 		{
@@ -17,7 +17,7 @@ void print_to_98(int n)
 		printf("98\n");
 		return;
 	}
-	
+
 	for (i = n; i < 98; i++)
 	{
 		printf("%d, ", i);
