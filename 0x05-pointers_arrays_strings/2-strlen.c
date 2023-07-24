23db0e1bd851c37e88b5f6e679ebff60c8ad6ceb
Mohamed <mdahni06@gmail.com>
2023-07-24 23:59:09 +0100

    added 2-strlen.c program

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be evaluated
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while(s)
+	{
+		length++;
+		s++;
+	}
+
+	return length;
+}
