56b2a775e2bf702c473893a2a5d387666f8e37b4
Mohamed <mdahni06@gmail.com>
2023-07-25 00:29:30 +0100

    added 5-rev_string.c program

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+void swap(char *a, char *b)
+{
+	char tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while(*s)
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
+
+void rev_string(char *s)
+{
+	int s_length = _strlen(s), i;
+
+	for (i = 0; i < s_length/2; i++)
+	{
+		swap(s[i], s[s_length-i-1]);
+	}
+}
