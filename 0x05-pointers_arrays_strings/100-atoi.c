6bc3bd53be5d5da470172d69f0a813e8ed1cb6f4
Mohamed <mdahni06@gmail.com>
2023-07-25 02:24:21 +0100

    added 100-atoi.c program

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -19,21 +19,39 @@ int is_number(char c)
  */
 int _atoi(char *s)
 {
-	int num = 0;
-	int sign = 1;
-	int found = 0;
-	while (*s)
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		if (is_number(*s))
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			found = 1;
-			num = (num * 10 * sign) + (*s - '0');
-		} else if (found) {
-			break;
-		} else if (*s == '-') {
-			sign *= -1;	
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
-		s++;
+		i++;
 	}
-	return (num);
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
