692a10057dfede5c63c38aeda0f40810fdba06f6
Mohamed Dahni <mdahni06@gmail.com>
2023-08-09 00:50:01 +0100

    +

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ int create_array(unsigned int size, char c)
 {
     if (size <= 0) return NULL;
 
-    char *arr = malloc(sizeof(char) * size);    
+    char *arr = (char*) malloc(sizeof(char) * size);    
     if (arr == NULL)
     {
         return NULL;
