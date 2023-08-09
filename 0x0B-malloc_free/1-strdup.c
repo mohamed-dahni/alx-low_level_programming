7f50105d7c3bcc78a5e0253427510195070aaaeb
Mohamed Dahni <mdahni06@gmail.com>
2023-08-09 01:31:44 +0100

    added 1-strdup.c program

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - duplicate a string
+ * @str: string to copy
+ * Return: duplicated string of str
+ */
+char *_strdup(char *str)
+{
+	char *dup_str = NULL;
+	int i = 0,
+		size = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i++])
+	{
+		size++;
+	}
+
+	dup_str = (char *) malloc(sizeof(char) * size);
+	if (dup_str == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		dup_str[i] = str[i];
+	dup_str[i] = '\0';
+
+
+	return (dup_str);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,2 +1,2 @@
-void _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
