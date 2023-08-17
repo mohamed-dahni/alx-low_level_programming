350721fe070667aa7d9c08621e751ecec7d77299
Mohamed Dahni <76598605+mohamed-dahni@users.noreply.github.com>
2023-08-17 15:52:45 +0000

    +

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,2 +1,8 @@
-void _putchar(char c);
-void print_name(char *name, void (*f)(char *));
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stddef.h>
+int _putchar(char);
+void print_name(char *, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *, int, int (*cmp)(int));
+#endif
