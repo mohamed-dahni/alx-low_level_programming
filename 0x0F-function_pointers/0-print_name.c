e925975411b54780ee7bf6c25c72021ff33e39e4
Mohamed Dahni <76598605+mohamed-dahni@users.noreply.github.com>
2023-08-17 16:00:48 +0000

    add the function pointers project

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
+#include <stdlib.h>
 #include "function_pointers.h"
+
 /**
- *print_name - Function that prints a name
- *@name: name of to be printed
- *@f: pointer to function
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-	{
+	if (!name || !f)
 		return;
-	}
-
 	f(name);
 }
