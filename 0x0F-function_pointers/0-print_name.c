f8356554e58aa6d2d1d0462657cbb36e4917b5cd
Mohamed Dahni <76598605+mohamed-dahni@users.noreply.github.com>
2023-08-17 15:33:18 +0000

    +

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,17 +2,13 @@
 #include "function_pointers.h"
 
 /**
- * print_name - print the return value of f function
+ * print_name - prints a name
  * @name: name to print
  * @f: pointer to the printing function
- * Return: Void
-*/
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!f || !name)
-	{
+	if (!name || !f)
 		return;
-	}
-
 	f(name);
 }
