9dae2409379b3217a890e947bb1f404bfcf72ae8
Mohamed Dahni <mdahni06@gmail.com>
2023-08-25 05:30:37 +0100

    add add node end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_node - add a node to the begenning of a singly linked list
+ * @head: the head of the singly linked list
+ * @str: the value of the node
+ * Return: the address of the newly created node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = NULL, aux = *head;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	new = (list_t *) malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdupt(str);
+	new->next = NULL;
+
+    while (aux->next != NULL)
+        aux = aux->next;
+
+    aux->next = new;
+
+	return (new);
+}
