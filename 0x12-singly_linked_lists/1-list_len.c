1de318af4437bb860369a6fd08901834ae6ac795
Mohamed Dahni <mdahni06@gmail.com>
2023-08-25 04:51:56 +0100

    add 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * list_len - counts how many elements in a singly linked list
+ * @h: the head of the singly linked list
+ * Return: How many elements in a singly linked list
+ */
+
+size list_len(const list_t *h)
+{
+	if (h == NULL)
+		return (0);
+
+	return (list_len(h->next) + 1);
+}
+
