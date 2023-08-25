e82e123dd6da2e59c6deca858336431a5d6a0d46
Mohamed Dahni <mdahni06@gmail.com>
2023-08-25 17:19:25 +0100

    +

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+/**
+ * first - print something before executing
+ */
 void first(void) __attribute__((constructor));
 
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
