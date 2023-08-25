a9a57aa7c3db1e8118a7c95c754d5fcf7070ae87
Mohamed Dahni <mdahni06@gmail.com>
2023-08-25 04:44:43 +0100

    +

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,14 +8,13 @@
  */
 size_t print_list(list_t *h)
 {
-  if (h == NULL)
-    return (0);
+	if (h == NULL)
+		return (0);
 
-  if (h->str == NULL)
-    printf("[0] (nil)\n");
-  else
-    printf("[%d] %s\n", h->len, h->str);
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("[%d] %s\n", h->len, h->str);
 
-  return (print_list(h->next) + 1);
+	return (print_list(h->next) + 1);
 }
-
