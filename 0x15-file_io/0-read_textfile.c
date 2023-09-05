38529de1c86ef341e9fb863c745075a9754cd8c7
Mohamed Dahni <mdahni06@gmail.com>
2023-09-05 14:21:43 +0100

    +

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,18 +11,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t o, r, w;
 	char *buffer;
 
-	if (filename == NULL || letters < 1)
+	if (filename == NULL)
 		return (0);
 
-	buffer = (char *) malloc(sizeof(char) * letters);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
-	w = write(STDIN_FILENO, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-	if (o == -1 || r == -1 || w != r)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
