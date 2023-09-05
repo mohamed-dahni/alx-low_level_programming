86f8c90a510098f96985b132ab945b4d5fd2f84c
Mohamed Dahni <mdahni06@gmail.com>
2023-09-05 13:52:16 +0100

    +

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,26 +15,26 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	/* handle unvalid arguments */
 	if (filename == NULL || letters < 1)
-		return (0);
+		return (-1);
 
 	/* open the file and check for failure */
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
+		return (-1);
 
 	/* read the file and check for failure */
 	blen = read(fd, buf, letters);
 	if (blen <= 0)
-		return (0);
+		return (-1);
 
 	/* write the file to stdout and check for failure */
 	blen = write(STDIN_FILENO, buf, letters);
 	if (blen <= 0)
-		return (0);
+		return (-1);
 
 	free(buf);
 	close(fd);
 
-	return (blen);
+	return (1);
 }
 
