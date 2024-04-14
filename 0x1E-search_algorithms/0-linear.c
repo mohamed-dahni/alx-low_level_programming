fe94d4b2f4a4e4e1729855b257f772cc5ab40f5e
Mohamed <mdahni06@gmail.com>
2024-04-14 16:30:47 +0100

    +

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -21,7 +21,6 @@ int linear_search(int *array, size_t size, int value)
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
-			printf("Found %d at index %ld\n", value, i);
 			return (i);
 		}
 	}
